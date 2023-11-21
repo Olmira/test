a1e312010616cbbabe35719a8cd2b748e9afd12b
Olmira <olmbel@gmail.com>
2023-11-21 16:29:59 +0100

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -1,38 +1,35 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.title('Hello Wilders, welcome to my application!')
 
-st.write("I enjoy to discover stremalit possibilities")
+st.write("voici la df entière")
 
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
-df_weather = pd.read_csv(link)
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df_car = pd.read_csv(link)
 
 # Here we use "magic commands":
-df_weather
+df_car
 
-st.line_chart(df_weather['MAX_TEMPERATURE_C'])
-
+st.write("voici la heatmap des correlations pour toutes les régions confundus")
 import seaborn as sns
-viz_correlation = sns.heatmap(df_weather.select_dtypes(include=np.number).corr(), 
+viz_correlation = sns.heatmap(df_car.select_dtypes(include=np.number).corr(), 
 								center=0,
 								cmap = sns.color_palette("vlag", as_cmap=True)
 								)
 
 st.pyplot(viz_correlation.figure)
+plt.close()
+
+st.write("voici la df par région")
+options = list(df_car['continent'].unique())
+choix_region = st.selectbox('choisisser la region: ', options)
+df_region = df_car[(df_car['continent']==choix_region)]
 
 
-import streamlit as st
-
-st.title('Hello Wilders, welcome to my application!')
-
-name = st.text_input("Please give me your name:")
-name_length = len(name)
-st.write("Your name has ",name_length, "characters")
-
-
-np. __version__
-sns. __version__
-pd. __version__
-st. __version__
+df_region
+st.write("voici la df par région", choix_region)
+sns.boxplot(x = df_region['cubicinches']).figure
+plt.close()
